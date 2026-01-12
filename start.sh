d933b3c7d82cc0c2d2d56346795639d306a8e35c
zrh-pi2 <894945409@qq.com>
2026-01-12 23:44:44 +0800

    config ch

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # === 必须的环境变量 ===
-export COMMAND_FORWARD_URL="http://127.0.0.1:8089/send/message"
+export COMMAND_FORWARD_URL="http://192.168.1.123:8089/send/message"
 export COMMAND_FORWARD_TIMEOUT="5"
 
 # === 初始化 conda ===
