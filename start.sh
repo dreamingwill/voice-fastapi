ce35ed30d035194bc50a243a916bafb55d2f2712
zrh-pi1 <ruihaozheng@qq.com>
2026-01-13 17:03:24 +0800

    更改目标端口地址

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 set -e
 
 # === 必须的环境变量 ===
-export COMMAND_FORWARD_URL="http://127.0.0.1:8089/send/message"
+#export COMMAND_FORWARD_URL="http://127.0.0.1:8089/send/message"
+export COMMAND_FORWARD_URL="http://192.168.20.13:9010/task/voice/run"
 export COMMAND_FORWARD_TIMEOUT="5"
 
 # === 初始化 conda ===
